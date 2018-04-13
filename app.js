517b035c2d21c8d3fd40750ba76acafb4abd58a4
DESKTOP-1LI8HSU\Omar <omarfawzi96@gmail.com>
2018-04-13 21:53:09 +0200

    adding mongoose to app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// connect with mongoose
+mongoose.connect(process.env.DB_HOST+process.env.DB_NAME);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

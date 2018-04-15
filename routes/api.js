bb1ae6f8f5fb4af02de317de2c59f40529556202
Omar Salama <omarfawzi96@gmail.com>
2018-04-16 01:41:58 +0200

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ require('express-reverse')(router);
 var BiographyController = require('../controllers/BiographyController');
 var VideosController = require('../controllers/VideosController');
 var GuestsController = require('../controllers/GuestsController');
+
 router.get('biography_api','/biography/get',BiographyController.prototype.getFakeBiography) ;
 router.get('videos_api','/videos/get',VideosController.prototype.getFakeVideos) ;
 router.get('guests_api','/guests/get',GuestsController.prototype.getFakeGuests) ;

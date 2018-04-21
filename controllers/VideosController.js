870fdb8011e2cf1f5918f6535e45282181b6c51b
DESKTOP-1LI8HSU\Omar <omarfawzi96@gmail.com>
2018-04-21 23:55:17 +0200

    add limit and offset for VideosController

--- controllers/VideosController.js
+++ controllers/VideosController.js
@@ -6,7 +6,13 @@ class VideosController {
 
     getFakeVideos(req,res){
         VideosFaker.prototype.generateSchema();
-        VideosFaker.prototype.printSchema().then(res.send.bind(res)).catch(console.error);
+        VideosFaker.prototype.printSchema().then(function (result) {
+            let limit = parseInt(req.query.limit || result.length);
+            let offset = parseInt(req.query.offset || 0);
+            return result.slice(offset, limit + offset);
+        }, function (err) {
+            console.log(err);
+        }).then(res.send.bind(res));
     }
 }
 

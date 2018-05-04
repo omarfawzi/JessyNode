2fce7bf3dfe8d200842c96c842058deab53697ce
DESKTOP-1LI8HSU\Omar <omarfawzi96@gmail.com>
2018-05-05 00:41:21 +0200

    upgrade videos generator

--- controllers/VideosController.js
+++ controllers/VideosController.js
@@ -9,7 +9,6 @@ class VideosController extends Controller {
     }
 
     getFakeData(req,res){
-        FakerFactory.prototype.getFaker(this.faker).prototype.generateSchema();
         FakerFactory.prototype.getFaker(this.faker).prototype.printSchema().then(result => {
             result = this.parseLinks(result);
             let limit = parseInt(req.query.limit || result.length);
@@ -21,12 +20,16 @@ class VideosController extends Controller {
     }
 
     parseLinks(results){
-        let hold = 1;
         let constVid = 'https://www.youtube.com/embed/';
         let newRes = [];
-        for (let i = 0 ; i < results[0].length ; i++,hold++){
-            results[0][i].link = constVid + results[hold];
-            newRes.push(results[0][i]);
+        for (let i = 0 ; i < results.length ; i++){
+            let obj = {} ;
+            obj.id = results[i].id.videoId;
+            obj.title = results[i].snippet.title;
+            obj.preview = results[i].snippet.thumbnails.default.url;
+            obj.link = constVid + obj.id;
+            obj.duration = Math.floor(Math.random() * Math.floor(10));
+            newRes.push(obj);
         }
         return newRes;
     }
--- fakers/VideosFaker.js
+++ fakers/VideosFaker.js
@@ -10,51 +10,15 @@ class VideosFaker extends Faker {
     }
 
     generateSchema() {
-        this.schema = {
-            "type": "array",
-            "properties": {
-                "video": {
-                    "id": {
-                        "type": "integer",
-                        "minimum": 0,
-                        "maximum": 100
-                    },
-                    "preview": {
-                        "type": "string",
-                        "faker": "image.avatar"
-                    },
-                    "title": {
-                        "type": "string",
-                        "faker": "lorem.sentence",
-                        "maxLength": 15
-                    },
-                    "duration": {
-                        "type": "integer",
-                        "maximum": 20,
-                        "minimum": 1
-                    }
-                }
-            },
-            "items": {
-                "$ref": "#/properties/video"
-            },
-            "minItems": 6,
-            "maxItems": 6
-        };
+
     }
 
     printSchema() {
-        var fakeSchema = new Promise((resolve, reject) => {
-            jsf.resolve(this.schema).then(function (result) {
-                resolve(result);
-            });
-        });
         let promises = [];
-        promises.push(fakeSchema);
         for (var i = 0; i < 6; i++) {
             promises.push(new Promise((resolve) => {
                     youtube.getRandomVid('AIzaSyAvIAy_smbYq_GCbWuyzqKIZqcRFZ7UgeU', function (err, data) {
-                        resolve(data.id.videoId);
+                        resolve(data);
                     });
                 })
             );

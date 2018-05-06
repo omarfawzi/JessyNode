c65f29c7ca2db171ff1b97ebae75becd8f1e3c0a
Omar Salama <omarfawzi96@gmail.com>
2018-05-06 11:32:51 +0200

    swap req,res

--- routes/api.js
+++ routes/api.js
@@ -2,32 +2,32 @@ var router = require('express')();
 require('express-reverse')(router);
 var ControllerFactory = require('../factories/ControllerFactory');
 
-router.get('biography_api','/biography/get',(res,req) => {
-    new ControllerFactory().getController('Biography').getFakeData(res,req);
+router.get('biography_api','/biography/get',(req,res) => {
+    new ControllerFactory().getController('Biography').getFakeData(req,res);
 }) ;
 
-router.get('videos_api','/videos/get',(res,req) => {
-    new ControllerFactory().getController('Videos').getFakeData(res,req);
+router.get('videos_api','/videos/get',(req,res) => {
+    new ControllerFactory().getController('Videos').getFakeData(req,res);
 }) ;
 
-router.get('guests_api','/guests/get',(res,req) => {
-    new ControllerFactory().getController('Guests').getFakeData(res,req);
+router.get('guests_api','/guests/get',(req,res) => {
+    new ControllerFactory().getController('Guests').getFakeData(req,res);
 }) ;
 
-router.get('banner_api','/banner/get',(res,req) => {
-    new ControllerFactory().getController('Banner').getFakeData(res,req);
+router.get('banner_api','/banner/get',(req,res) => {
+    new ControllerFactory().getController('Banner').getFakeData(req,res);
 }) ;
 
-router.get('daily_tweets_api','/dailyTweets/get',(res,req) => {
-    new ControllerFactory().getController('DailyTweets').getFakeData(res,req);
+router.get('daily_tweets_api','/dailyTweets/get',(req,res) => {
+    new ControllerFactory().getController('DailyTweets').getFakeData(req,res);
 }) ;
 
-router.get('testimonials_api','/testimonials/get',(res,req) => {
-    new ControllerFactory().getController('Testimonials').getFakeData(res,req);
+router.get('testimonials_api','/testimonials/get',(req,res) => {
+    new ControllerFactory().getController('Testimonials').getFakeData(req,res);
 }) ;
 
-router.get('gallery_api','/gallery/get',(res,req) => {
-    new ControllerFactory().getController('Gallery').getFakeData(res,req);
+router.get('gallery_api','/gallery/get',(req,res) => {
+    new ControllerFactory().getController('Gallery').getFakeData(req,res);
 }) ;
 
 router.post('subscribe_api','/subscribe/post',new ControllerFactory().getController('Subscribe').subscribe);
